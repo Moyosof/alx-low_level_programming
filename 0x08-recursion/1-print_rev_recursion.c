d97b73a54ba3b699fc496bc58053043671d0bcc3
Moyosof <iamthaizzie@gmail.com>
2022-04-28 18:26:32 +0100

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,15 @@
 
 
 /**
- * _strlen_recursion - get length of string
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer to a string
- * Return: length of string
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
+
 	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
